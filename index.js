8cfe335f076c0a1d903b8947c6badf89aafd02a6
RAVIVERMA6393 <147376604+RAVIVERMA6393@users.noreply.github.com>
2024-04-08 11:02:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// q.1
+
+// let marks = parseInt(prompt('enter the marks'));
+// let grade;
+// switch(true){
+//     case marks >=41 && marks<=50: grade='A'
+//     break;
+//     case marks >=31 && marks <=40: grade='B'
+//     break;
+//     case marks >=21 && marks <=30: grade='C'
+//     break;
+//     case marks >=11 && marks <=20: grade='D'
+//     break;
+//     default: grade='E'
+// }
+// console.log(grade);
+
+
+// Q.2
+
+// let word = prompt('enter the word');
+// if(word == 'P' || word == 'p'){
+//     console.log('prepbytes')
+// }
+// else if(word == 'Z' || word == 'z'){
+//     console.log('Zenith')
+// }
+// else if(word == 'E' || word == 'e'){
+//     console.log('Expert Coder')
+// }
+// else if(word == 'D' || word == 'd'){
+//     console.log('Dats Structure')
+// }
+
+
+// Q.3
+
+// let A = prompt('enter no. A')
+// let B = prompt('enter no. B')
+// let C = prompt('enter no. C')
+// if(A > B && A > C){
+//     console.log(A)
+// }
+// else if (B > C && B > A){
+//     console.log(B)
+// }
+// else{
+//     console.log(C)
+// }
+
+// Q.4
+
+// let A = prompt('enter no. A')
+// let B = prompt('enter no. B')
+// let C = prompt('enter no. C')
+// if((A <=B && A<=C) || (C<=A && C<=B)){
+//     console.log(B)
+// }
+// else if((B <= A && B <= C) || (C <= A && A<=B)){
+//     console.log(A)
+// }
+// else{
+//     console.log(C)
+// }
+
+
+// Q.5 check triangle
+
+// let a1 = parseInt(prompt('enter no. a1'))
+// let b1 = parseInt(prompt('enter no. b1'))
+// let c1 = parseInt(prompt('enter no. c1'))
+// if(a1*a1 + b1*b1 > c1*c1){
+//     console.log('Acute angle')
+// }else if(a1*a1 + b1*b1 < c1*c1){
+//     console.log('obtuse angle')
+// }
+// else{
+//     console.log('not a angle')
+// }
